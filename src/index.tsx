db5e39bb2e9494d8f63c317d074e70f6960a1a1f
Teddy-FN <tedd.ferdy@gmail.com>
2021-10-12 14:04:40 +0700

    customize dots position

--- src/index.tsx
+++ src/index.tsx
@@ -336,13 +336,13 @@ const styles = StyleSheet.create({
     bottom: 16,
     left: 16,
     right: 16,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
   paginationDots: {
     height: 16,
     margin: 16,
     flexDirection: isAndroidRTL ? 'row-reverse' : 'row',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
   },
   dot: {
